b1ae78ffe7e3c32a5d2c9959d8db455a027f8c2c
yadavyogendra28 <yogsingh281199@gmail.com>
2026-06-07 12:46:44 +0530

    restructuring

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -20,7 +20,6 @@ export default function RootLayout({ children }) {
   return (
     <html
       lang="en"
-      data-theme="obsidian"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">{children}</body>
--- src/app/(app)/job-search/page.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const JobSearch = () => {
-  return (
-    <div>JobSearch</div>
-  )
-}
-
-export default JobSearch
